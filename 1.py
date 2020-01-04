e8956f1b9a18561dacebc5e57be461e4c66c6845
Kirill Lezhnin <klezhnin@tigercpu.princeton.edu>
2020-01-04 15:59:09 -0500

    updated all to fix documentation

--- 1.py
+++ 1.py
@@ -1,15 +1,16 @@
-""" @package 1
+""" @package 1.py
  Trying to document 1.py
 """
 
-"""
- increment function
-"""
+
 def inc(x):
+    """
+    increment function
+    """
     return x + 1
 
-"""
- increment function test
-"""
 def test_answer():
+    """
+    increment function test
+    """
     assert inc(3) == 4
