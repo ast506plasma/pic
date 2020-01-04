e1c31215ccf24331ff961f640bb5d54ccb4a11a7
Kirill Lezhnin <klezhnin@tigercpu.princeton.edu>
2020-01-04 15:49:30 -0500

    1.py commented

--- 1.py
+++ 1.py
@@ -1,6 +1,15 @@
+""" @package 1
+ Trying to document 1.py
+"""
+
+"""
+ increment function
+"""
 def inc(x):
     return x + 1
 
-
+"""
+ increment function test
+"""
 def test_answer():
     assert inc(3) == 4
