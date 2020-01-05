854e672ece83ec9633ba0eee59852a1047b8b9f4
sirpipat <sirpipat@umich.edu>
2020-01-05 17:43:25 -0500

    Updated test cases

--- test_shape.py
+++ test_shape.py
@@ -4,14 +4,23 @@ import numpy as np
 # Maximum error when comparing float numbers
 PRECISION = 1e-5
 
-def test_shape():
-	s = Shape1DTriangle(2)
+def test_shape_basic():
+	s = Shape1DTriangle(1)
 	assert s._Ndims == 1
 	assert s._type == 'Triangle'
+	assert s._size == 1
+
+def test_shape_set_shape():
+	s = Shape1DTriangle(3)
+	s.set_shape(2)
 	assert s._size == 2
 	
-	particle_position = 1
 
+def test_shape_get_height():
+	s = Shape1DTriangle(2)
+	
+	particle_position = 1
+	
 	assert np.abs(s.get_height(1, particle_position) - 0.5) < PRECISION
 	assert np.abs(s.get_height(0, particle_position) - 0.25) < PRECISION
 	assert np.abs(s.get_height(2, particle_position) - 0.25) < PRECISION
