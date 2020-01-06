1f312e6ec0c4d752949e7f5aa65c52cedeadf681
klezhnin42 <klezhnin@princeton.edu>
2020-01-06 18:10:05 -0500

    updated indents in grid.py

--- Grid.py
+++ Grid.py
@@ -12,23 +12,23 @@ class Grid:
 		"""
 		The constructor
 		"""
-        	self._grid_step = grid_step
-        	self._grid_size = grid_size
-
+       		self._grid_step = grid_step
+       		self._grid_size = grid_size
+		
 	def set_grid(self, grid_step, grid_size):
 		"""
 		Set the grid after it is initialized
 		""" 
 		pass
-
+	
 	def set_grid_container(self, grid, is_shifted, grid_container):
 		"""
 		Setting values of grid container (i.e. fields or sources)
 		"""
 		pass
-
-        def get_grid_container(self, grid, is_shifted, grid_container):
-                """
-                Accessing values of grid container (i.e. fields or sources)
-                """
-                pass
+	
+	def get_grid_container(self, grid, is_shifted, grid_container):
+		"""
+		Accessing values of grid container (i.e. fields or sources)
+		"""
+		pass
