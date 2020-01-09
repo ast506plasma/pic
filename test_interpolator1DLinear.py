bea57b4c9f6a51149e7765b4aba2845bb43889a5
sirpipat <sirpipat@umich.edu>
2020-01-08 23:15:46 -0500

    Change Grid class to mock version

--- test_interpolator1DLinear.py
+++ test_interpolator1DLinear.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+from unittest.mock import MagicMock
 from Grid1DCartesian import *
 from Interpolator1DLinear import *
 import numpy as np
@@ -7,25 +9,17 @@ import numpy as np
 #      y = np.array([1.0, 3.0, 5.0, 7.0, 9.0])
 # Since this function is linear, the interpolated value should equal
 # the exact value.
-def create_grid_basic():
-	grid_step = np.array([1.0])
-	grid_size = np.array([6])
-	# Create an empty grid
-	grid = Grid1DCartesian(grid_step, grid_size)
-	grid.set_grid()
-	# Create the container
-
-	x = np.arange(0.5, (grid_size[-1] - 0.5), 1.0) * grid_step[-1]
-	container = 2 * x
-	grid.set_grid_container(container)
-
-	return grid
-
 def test_interpolator_basic():
 	interpolator = Interpolator1DLinear()
-	pos = np.arange(1, 5, 1)
-	field = create_grid_basic()
 
+	# Create a mock grid that represent a field
+	x = np.array([0.5, 1.5, 2.5, 3.5, 4.5])
+	y = 2 * x
+	field = Mock()
+	field.get_grid_shifted = MagicMock(return_value = x)
+	field.get_grid_container = MagicMock(return_value = y)
+	
+	pos = np.arange(1, 5, 1)
 	# interpolated field for all position
 	field_i = interpolator(pos, field)
 
