451cb7102268e54563a4836bd3a82d857d79bbdc
Kirill Lezhnin <klezhnin@tigercpu.princeton.edu>
2020-01-09 21:26:12 -0500

    Is_shifted & Ndims getters implemented

--- Grid.py
+++ Grid.py
@@ -8,13 +8,14 @@ class Grid:
 	"""
 	Documentation for Grid class.
 	"""
-	def __init__(self, grid_step, grid_size):
+	def __init__(self, grid_step, grid_size, is_shifted):
 		"""
 		The constructor which delivers data of grid steps and number of nodes
 		in the specific direction
 		"""
 		self._grid_step = grid_step
 		self._grid_size = grid_size
+		self._is_shifted = is_shifted
 
 	def set_grid(self, grid_step, grid_size):
 		"""
@@ -27,13 +28,19 @@ class Grid:
 		Returns the grid
 		"""
 		return self._grid
-	
+
 	def get_grid_shifted(self):
 		"""
 		Returns the shifted grid
 		"""
 		return self._grid_shifted
-	
+
+	def get_if_shifted(self):	
+		"""
+		Returns the is_shifted, telling whether the grid is shifted or not
+		"""
+		return self._is_shifted
+
 	def set_grid_container(self, grid_container):
 		"""
 		Setting values of grid container (i.e. fields or sources)
@@ -45,3 +52,9 @@ class Grid:
 		Accessing values of grid container (i.e. fields or sources)
 		"""
 		return self._grid_container
+
+	def get_Ndims(self):
+		"""
+		returning number of dimensions for the field solver purposes
+		"""
+		return self._Ndims
