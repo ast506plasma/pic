53e893192d948a9f56538e838854c94f5b738924
Nijaid Arredondo <josena@princeton.edu>
2020-01-13 11:31:02 -0500

    Update Pusher.py
    Co-Authored-By: Sirawich Pipatprathanporn <sirpipat@umich.edu>

--- Pusher.py
+++ Pusher.py
@@ -30,7 +30,7 @@ class Pusher1DLeapFrog(Pusher):
         for pp in collection.particles:
             # Make sure all particles are of the same type
             assert pp.type == firsttype,\
-                   "Particles in collection are not of the same type")
+                   "Particles in collection are not of the same type"
 
             force = 0.0
             posidx = pp.get_closest(gridhalf)
