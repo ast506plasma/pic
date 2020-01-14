686f5490e33020885e83c2b4414df217ce8a72ca
Kirill Lezhnin <klezhnin@tigercpu.princeton.edu>
2020-01-14 17:30:33 -0500

    output changed

--- pic/output.py
+++ pic/output.py
@@ -1,6 +1,8 @@
 """@package Output
 """
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 def full_output(field,epc,ipc,datadir,numstep):
 	"""
@@ -21,3 +23,55 @@ def full_output(field,epc,ipc,datadir,numstep):
 	np.savetxt(datadir+'ex_'+str(numstep)+'.out',field.ex, delimiter=',')
 	np.savetxt(datadir+'ele_'+str(numstep)+'.out',np.array(list(zip(elex,elepx))))
 	np.savetxt(datadir+'ion_'+str(numstep)+'.out',np.array(list(zip(ionx,ionpx))))
+
+
+def postprocess(datadir,xe, pex, grid, ex, dt, i,xmax):
+	"""
+	This function plots a bunch of figures from the raw output files
+	"""
+	#histograms of all particles in x-pex space
+	plt.clf()
+	plt.hist2d(xe, pex, bins=(40, 40), cmap=plt.cm.jet)
+	plt.colorbar()
+	plt.clim(0,40)
+	plt.xlim([0,10])
+	plt.xlabel(r'$x/d_{e0}$')
+	plt.ylabel(r'$p_{ex}/m_ec$')
+	omegape=r'$\,\omega_{pe0}^{-1}$'
+	plttitle="t="+str(int(dt*i*10000.0)/10000.0)+omegape
+	plt.title(plttitle)
+	plt.savefig(datadir+'x_px_'+str(i)+'.png')
+
+	#trajectories of all particles in x-pex space
+	plt.clf()
+	plt.plot(xe, pex,'.')
+	plt.xlim([0,10])
+	plt.xlabel(r'$x/d_{e0}$')
+	plt.ylabel(r'$p_{ex}/m_ec$')
+	omegape=r'$\,\omega_{pe0}^{-1}$'
+	plttitle="t="+str(int(dt*i*10000.0)/10000.0)+omegape
+	plt.title(plttitle)
+	plt.savefig(datadir+'traj_x_px_'+str(i)+'.png')
+
+
+	#electric field Ex
+	plt.clf()
+	plt.plot(grid, ex)
+	plt.xlim([0,10])
+	plt.xlabel(r'$x/d_{e0}$')
+	plt.ylabel(r'$E_{x}$')
+	omegape=r'$\,\omega_{pe0}^{-1}$'
+	plttitle="t="+str(int(dt*i*10000.0)/10000.0)+omegape
+	plt.title(plttitle)
+	plt.savefig(datadir+'ex_'+str(i)+'.png')
+
+	#FFT(Ex)
+	plt.clf()
+	plt.plot([2.0*np.pi/xmax*i for i in range(len(ex))],[np.abs(xx) for xx in np.fft.fft(ex)])
+	plt.xlabel(r'$k_x d_{e0}$')
+	plt.ylabel(r'$|E_{x}^k|$')
+	plt.yscale('log')
+	omegape=r'$\,\omega_{pe0}^{-1}$'
+	plttitle="t="+str(int(dt*i*10000.0)/10000.0)+omegape
+	plt.title(plttitle)
+	plt.savefig(datadir+'fftex_'+str(i)+'.png')
