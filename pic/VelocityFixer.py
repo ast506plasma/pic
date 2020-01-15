353d8d91fe280ad6f7bcd74db128383e43e9d1a3
sirpipat <sirpipat@umich.edu>
2020-01-14 22:06:33 -0500

    Fixed the location of force_func

--- pic/VelocityFixer.py
+++ pic/VelocityFixer.py
@@ -34,13 +34,12 @@ class VelocityFixer1DLeapFrog(VelocityFixer):
             assert pp.type == firsttype,\
                    "Particles in collection are not of the same type"
 
+            def force_func(gridpos, pos):
+                return 1.0-np.abs(gridpos - pos) / grid_step
+
             force = 0.0
             posidx = pp.get_closest(gridhalf)
             force = field.ex[posidx] * force_func(gridhalf[posidx], pp.position)
-
-
-            def force_func(gridpos, pos):
-                return 1.0-np.abs(gridpos - pos) / grid_step
 
             # Accelerate particle
             pp.momentum -= 0.5*field.time_step*force
